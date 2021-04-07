8cc8ac2cfd310edd48cf872df375b0a71e8aa763
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2021-04-07 14:42:28 +0200

    initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from 'react'; 
+
+function Note() {
+    return <div className="note">
+        <h1 contenteditable="true">Add title here</h1>
+        <p contenteditable="true">Add note here</p>
+    </div>
+}
+
+export default Note; 
